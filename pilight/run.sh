6c698bf3582c472890af8880f2827360d1c7651e
Philipp Schmitt <philipp@schmitt.co>
2017-10-19 22:01:12 +0200

    Fix sed syntax

--- pilight/run.sh
+++ pilight/run.sh
@@ -6,7 +6,7 @@ GPIO_PLATFORM=$(jq -r '.gpio_platform // empty' "$CONFIG_PATH")
 GPIO_PLATFORM=${GPIO_PLATFORM:-none} # default to none
 
 # Update pilight config
-sed -ir 's/("gpio-platform"): ".*"(.*)/\1: "'"$GPIO_PLATFORM"'"\2/' /etc/pilight/config.json
+sed -i 's/\("gpio-platform"\): \?".*"\(.*\)/\1: "'"$GPIO_PLATFORM"'"\2/' /etc/pilight/config.json
 
 # Run pilight in foreground
 exec pilight-daemon -D
