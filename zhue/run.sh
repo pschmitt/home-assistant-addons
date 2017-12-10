f1e7a9dd42791d6e6d2d962c911d9cdb228fee8f
Philipp Schmitt <philipp@schmitt.co>
2017-12-10 17:36:59 +0100

    Ignored _pressed events and bump version

--- zhue/run.sh
+++ zhue/run.sh
@@ -9,5 +9,7 @@ HUE_USERNAME=$(jq --raw-output ".hue_username" "$CONFIG_PATH")
 HUE_SWITCH=$(jq --raw-output ".hue_switch" "$CONFIG_PATH")
 
 # Run zhue
-exec python -m zhue.cli -b "$HUE_BRIDGE" -u "$HUE_USERNAME" switch "$HUE_SWITCH" -w \
+exec python -m zhue.cli -b "$HUE_BRIDGE" -u "$HUE_USERNAME" \
+  switch "$HUE_SWITCH" \
+  -w --ignore-pressed-events \
   -H "$HASS_URL" -P "$HASS_PASSWORD"
