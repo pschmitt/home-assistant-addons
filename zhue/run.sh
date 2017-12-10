2a14759467da4e3b87ae9ed3bbd8173468f8107a
Philipp Schmitt <philipp@schmitt.co>
2017-12-10 16:28:44 +0100

    Don't discard the hue bridge config option

--- zhue/run.sh
+++ zhue/run.sh
@@ -8,6 +8,6 @@ HUE_BRIDGE=$(jq --raw-output ".hue_bridge" "$CONFIG_PATH")
 HUE_USERNAME=$(jq --raw-output ".hue_username" "$CONFIG_PATH")
 HUE_SWITCH=$(jq --raw-output ".hue_switch" "$CONFIG_PATH")
 
-# Run zabbix-agent in foreground
-exec python -m zhue.cli -u "$HUE_USERNAME" switch "$HUE_SWITCH" -w \
+# Run zhue
+exec python -m zhue.cli -b "$HUE_BRIDGE" -u "$HUE_USERNAME" switch "$HUE_SWITCH" -w \
   -H "$HASS_URL" -P "$HASS_PASSWORD"
