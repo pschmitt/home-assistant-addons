8b55137017b027d74687fa44c9b42fba130fde41
Philipp Schmitt <philipp@schmitt.co>
2017-12-10 13:14:36 +0100

    Add zhue addon

---
+++ zhue/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Extract config data
+CONFIG_PATH=/data/options.json
+HASS_URL=$(jq --raw-output ".hass_url" "$CONFIG_PATH")
+HASS_PASSWORD=$(jq --raw-output ".hass_password" "$CONFIG_PATH")
+HUE_BRIDGE=$(jq --raw-output ".hue_bridge" "$CONFIG_PATH")
+HUE_USERNAME=$(jq --raw-output ".hue_username" "$CONFIG_PATH")
+HUE_SWITCH=$(jq --raw-output ".hue_switch" "$CONFIG_PATH")
+
+# Run zabbix-agent in foreground
+exec python -m zhue.cli -u "$HUE_USERNAME" switch "$HUE_SWITCH" -w \
+  -H "$HASS_URL" -P "$HASS_PASSWORD"
