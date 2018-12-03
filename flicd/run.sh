387fc4fbda39cb85a9333cad2f5d1502d7d287fb
Igor Motov <igor@motovs.org>
2018-12-03 18:33:23 -0500

    Remove unnecessary check for empty HCI_DEV

--- flicd/run.sh
+++ flicd/run.sh
@@ -4,10 +4,5 @@
 CONFIG_PATH=/data/options.json
 HCI_DEV=$(jq --raw-output ".hci_dev // empty" "$CONFIG_PATH")
 
-if [[ -n "$HCI_DEV" ]]
-then
-  HCI_DEV_PARAM="-h $HCI_DEV"
-fi
-
 # Run flicd
 exec /usr/bin/flicd -f /data/flic-db -s "0.0.0.0" -p 5551 -h "$HCI_DEV"
