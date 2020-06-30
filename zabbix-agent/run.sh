1111ca1989fcda6dd05573810a76910e2114f288
Philipp Schmitt <philipp@schmitt.co>
2020-06-30 14:00:17 +0200

    Fix Zabbix Server if server config contains '/' chars (eg: 192.168.0.0/24)

--- zabbix-agent/run.sh
+++ zabbix-agent/run.sh
@@ -6,7 +6,7 @@ ZABBIX_SERVER=$(jq --raw-output ".server" "$CONFIG_PATH")
 ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
 
 # Update zabbix-agent config
-sed -i 's/^\(Server\(Active\)\?\)=.*/\1='"${ZABBIX_SERVER}"'/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's#^\(Server\(Active\)\?\)=.*#\1='"${ZABBIX_SERVER}"'#' /etc/zabbix/zabbix_agentd.conf
 sed -i 's/^\(Hostname\)=.*/\1='"${ZABBIX_HOSTNAME}"'/' /etc/zabbix/zabbix_agentd.conf
 
 # Run zabbix-agent in foreground
