9e7fe3fe14d489f04b540bdd08f214722cd8a958
Philipp Schmitt <philipp@schmitt.co>
2020-07-06 14:40:18 +0200

    Fix agent2 startup parameter

--- zabbix-agent2/run.sh
+++ zabbix-agent2/run.sh
@@ -11,4 +11,4 @@ sed -i 's/^\(Server\(Active\)\?\)=.*/\1='"${ZABBIX_SERVER}"'/' "$ZABBIX_CONFIG_F
 sed -i 's/^\(Hostname\)=.*/\1='"${ZABBIX_HOSTNAME}"'/' "$ZABBIX_CONFIG_FILE"
 
 # Run zabbix-agent2 in foreground
-exec su zabbix -s /bin/ash -c "zabbix_agent2 -f -v"
+exec su zabbix -s /bin/ash -c "zabbix_agent2 -f"
