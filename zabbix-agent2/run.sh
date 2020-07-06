5e001ffd62da0a5dc219a86e38287604707bd030
Philipp Schmitt <philipp@schmitt.co>
2020-07-06 14:38:12 +0200

    Fix config file path

--- zabbix-agent2/run.sh
+++ zabbix-agent2/run.sh
@@ -6,8 +6,9 @@ ZABBIX_SERVER=$(jq --raw-output ".server" "$CONFIG_PATH")
 ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
 
 # Update zabbix-agent config
-sed -i 's/^\(Server\(Active\)\?\)=.*/\1='"${ZABBIX_SERVER}"'/' /etc/zabbix/zabbix_agentd2.conf
-sed -i 's/^\(Hostname\)=.*/\1='"${ZABBIX_HOSTNAME}"'/' /etc/zabbix/zabbix_agentd2.conf
+ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agent2.conf
+sed -i 's/^\(Server\(Active\)\?\)=.*/\1='"${ZABBIX_SERVER}"'/' "$ZABBIX_CONFIG_FILE"
+sed -i 's/^\(Hostname\)=.*/\1='"${ZABBIX_HOSTNAME}"'/' "$ZABBIX_CONFIG_FILE"
 
 # Run zabbix-agent2 in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agent2 -f -v"
