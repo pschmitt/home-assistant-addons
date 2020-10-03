be067c7cf63a96d214e041ce087f7f08a5bbdf8d
Antoine <524706+Twanislas@users.noreply.github.com>
2020-10-03 11:36:42 +0200

    Fix ignored Hostname config (#32)
    Hostname might be commented out in the stock config file. Updated the regex to take it into account.

--- zabbix-agent2/run.sh
+++ zabbix-agent2/run.sh
@@ -8,7 +8,7 @@ ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
 # Update zabbix-agent config
 ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agent2.conf
 sed -i 's#^\(Server\(Active\)\?\)=.*#\1='"${ZABBIX_SERVER}"'#' "$ZABBIX_CONFIG_FILE"
-sed -i 's/^\(Hostname\)=.*/\1='"${ZABBIX_HOSTNAME}"'/' "$ZABBIX_CONFIG_FILE"
+sed -i 's/^#\?\s\?\(Hostname\)=.*$/\1='"${ZABBIX_HOSTNAME}"'/' "${ZABBIX_CONFIG_FILE}"
 
 # Run zabbix-agent2 in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agent2 -f"
