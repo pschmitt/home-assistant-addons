32215acafa1bf44f29b8bbf4db875a61cef8223c
Antoine <524706+Twanislas@users.noreply.github.com>
2020-10-05 14:44:29 +0200

    Fix regex

--- zabbix-agent2/run.sh
+++ zabbix-agent2/run.sh
@@ -7,7 +7,7 @@ ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
 
 # Update zabbix-agent config
 ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agent2.conf
-sed -i 's/^#\?\s?\(Server\(Active\)\?\)=.*/\1='"${ZABBIX_SERVER}"'/' "$ZABBIX_CONFIG_FILE"
+sed -i 's@^#\?\s\?\(Server\(Active\)\?\)=.*@\1='"${ZABBIX_SERVER}"'@' "$ZABBIX_CONFIG_FILE"
 sed -i 's/^#\?\s\?\(Hostname\)=.*$/\1='"${ZABBIX_HOSTNAME}"'/' "${ZABBIX_CONFIG_FILE}"
 
 # Run zabbix-agent2 in foreground
