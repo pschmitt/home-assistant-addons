0e57d5d8723e858e7a3532f781026a9ea7643dab
Philipp Schmitt <pschmitt@users.noreply.github.com>
2020-11-23 21:43:35 +0100

    Default to hci0
    Otherwise hci_dev might be unset.

--- flicd/run.sh
+++ flicd/run.sh
@@ -2,7 +2,7 @@
 
 # Extract config data
 CONFIG_PATH=/data/options.json
-HCI_DEV=$(jq --raw-output ".hci_dev // empty" "$CONFIG_PATH")
+HCI_DEV=$(jq --raw-output '.hci_dev // "hci0"' "$CONFIG_PATH")
 
 # Run flicd
 exec /usr/bin/flicd -f /data/flic-db -s "0.0.0.0" -p 5551 -h "$HCI_DEV"
