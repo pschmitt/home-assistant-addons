35b77b374553f5a3706a5442cae56cc7337c6565
lumarel <lukas@magauer.eu>
2023-07-09 22:00:00 +0200

    Fixes #66 - Zabbix Agent (2) Split Server/ServerActive variables

--- zabbix-agent/run.sh
+++ zabbix-agent/run.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env ash
+# shellcheck shell=dash
 
 # Extract config data
 CONFIG_PATH=/data/options.json
-ZABBIX_SERVER=$(jq --raw-output ".server" "$CONFIG_PATH")
-ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "$CONFIG_PATH")
+ZABBIX_SERVER=$(jq --raw-output ".server" "${CONFIG_PATH}")
+ZABBIX_SERVER_ACTIVE=$(jq --raw-output ".serveractive" "${CONFIG_PATH}")
+ZABBIX_HOSTNAME=$(jq --raw-output ".hostname" "${CONFIG_PATH}")
 
 # Update zabbix-agent config
-sed -i 's#^\(Server\(Active\)\?\)=.*#\1='"${ZABBIX_SERVER}"'#' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/^\(Hostname\)=.*/\1='"${ZABBIX_HOSTNAME}"'/' /etc/zabbix/zabbix_agentd.conf
+ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agentd.conf
+sed -i 's@^\(Server\)=.*@\1='"${ZABBIX_SERVER}"'@' "${ZABBIX_CONFIG_FILE}"
+sed -i 's@^\(ServerActive\)=.*@\1='"${ZABBIX_SERVER_ACTIVE}"'@' "${ZABBIX_CONFIG_FILE}"
+sed -i 's@^\(Hostname\)=.*@\1='"${ZABBIX_HOSTNAME}"'@' "${ZABBIX_CONFIG_FILE}"
 
 # Run zabbix-agent in foreground
 exec su zabbix -s /bin/ash -c "zabbix_agentd -f"
